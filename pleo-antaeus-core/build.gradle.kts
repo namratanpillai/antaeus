d9466785675ba0e939753356a431696a01287dfa
Namrata <namratanpillai@gmail.com>
2022-03-05 12:50:05 +0530

    #New libs

--- pleo-antaeus-core/build.gradle.kts
+++ pleo-antaeus-core/build.gradle.kts
@@ -4,7 +4,12 @@ plugins {
 
 kotlinProject()
 
+
 dependencies {
     implementation(project(":pleo-antaeus-data"))
+    implementation("org.jetbrains.exposed:exposed:0.17.7")
+    implementation("org.xerial:sqlite-jdbc:3.30.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.7")
+    
     api(project(":pleo-antaeus-models"))
 }
