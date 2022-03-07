10c63c00f0174286b86df68bb80877f4270ecedc
Namrata <namratanpillai@gmail.com>
2022-03-07 10:20:28 +0530

    #Adding status validation

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/AdhocPaymentService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/AdhocPaymentService.kt
@@ -1,5 +1,7 @@
 package io.pleo.antaeus.core.services
 
+import io.pleo.antaeus.core.exceptions.validations.DataValidationException
+import io.pleo.antaeus.core.utility.ErrorConstants
 import io.pleo.antaeus.models.InvoiceRequest
 import io.pleo.antaeus.models.InvoiceStatus
 import io.pleo.antaeus.models.external.PaymentResponse
@@ -28,18 +30,33 @@ class AdhocPaymentService(
      * Pass a list of invoices that needs to to paid ADHOC.
      * Use this in case payments based on country, date range and status
      */
+    @Throws(DataValidationException::class)
     fun adhocBilling(request: InvoiceRequest):List<PaymentResponse>{
 
-        val  statusList= mutableListOf(request.status!!)
-        if(statusList.isEmpty()){
-            statusList.addAll(mutableListOf(InvoiceStatus.FAILED.name,InvoiceStatus.PENDING.name))
-        }
-
+        val statusList=validateAllowedStatus(request.status!!)
         val invoicesToProcess=invoiceService.fetch(request.startDate!!,request.endDate!!,request.countryCode!!).filter { i-> statusList.contains(i.status.toString()) }
         val chunkedInvoices= invoicesToProcess.chunked(10)
 
         chunkedInvoices.parallelStream().map { i-> ChannelService(billingService).pushInvoiceForProcessing(i)}.collect(Collectors.toList<Unit>())
         return billingService.billCustomer(invoicesToProcess)
 
+    }
+
+    @Throws(DataValidationException::class)
+    private fun validateAllowedStatus(status:String):List<String>{
+
+        val allowedStatus=mutableListOf(InvoiceStatus.FAILED.name,InvoiceStatus.PENDING.name)
+        val  statusList= mutableListOf<String>()
+
+        if(status.isNotEmpty()){
+            if(!allowedStatus.contains(status)){
+                throw DataValidationException("",ErrorConstants.INVALID_STATUS,ErrorConstants.INVALID_STATUS_MESSAGE)
+            }else{
+                statusList.add(status)
+            }
+        }else{
+            statusList.addAll(allowedStatus)
+        }
+        return statusList
     }
 }
